391b9bf926d3320354ffc6fc056796da49e0884f
Bergis1610 <emilberglund39@gmail.com>
2023-03-01 10:21:31 +0100

    Third commit including all files from assignment3

---
+++ assignment3/task1.py
@@ -0,0 +1,13 @@
+import numpy as np
+import scipy
+
+x = np.array(  [[2, 1, 2, 3, 1],
+                [4, 5, 0, 7, 0],
+                [3, 9, 1, 1, 4]])
+
+y = np.array(  [[1, 0, -1], 
+                [2, 0, -2],
+                [1, 0, -1]])
+
+
+print(scipy.ndimage.convolve(x,y,))
